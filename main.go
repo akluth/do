5ab9b999ee454d8ebd968dcd3fcff20097c20e6e
Alexander Kluth <me@dittusch.dev>
2019-07-30 12:33:53 +0200

    Somewhat functional
    Signed-off-by: Alexander Kluth <me@dittusch.dev>

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"github.com/BurntSushi/toml"
 	"github.com/alexflint/go-arg"
+	"github.com/flynn/go-shlex"
 	. "github.com/logrusorgru/aurora"
 	"io/ioutil"
 	"log"
+	"os"
+	"os/exec"
+	"strings"
 )
 
 var args struct {
@@ -15,15 +19,46 @@ var args struct {
 
 type Dofile struct {
 	Description string
-	Tasks map[string]Task
+	Tasks map[string]task
 }
 
-type Task struct {
-	commands []string
+type task struct {
+	Commands []string
 }
 
-func executeTask() {
+func remove(slice []string, s int) []string {
+	return append(slice[:s], slice[s+1:]...)
+}
 
+func parseCommand(command string) []string {
+	parts, err := shlex.Split(strings.TrimSpace(command))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return parts
+}
+
+func executeTask(doFile Dofile, taskName string) {
+	if _, found := doFile.Tasks[taskName]; found {
+		fmt.Println(Bold(Green("Executing task")), Bold(Cyan(taskName)))
+
+		for _, command := range doFile.Tasks[taskName].Commands {
+			fmt.Println(Bold(Yellow(taskName)), " ",  command)
+
+			tokens := parseCommand(command)
+			cmdName := tokens[0]
+			tokens = remove(tokens, 0)
+
+			if err := exec.Command(cmdName, tokens...).Run(); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		}
+	} else {
+		fmt.Println(Bold(Red("Could not find task")), Bold(Yellow(taskName)), Bold(Red("aborting!")))
+		os.Exit(-1);
+	}
 }
 
 func main() {
@@ -40,6 +75,8 @@ func main() {
 	}
 
 	for _, taskName := range args.TaskName {
-		fmt.Println(Bold(Green("Executing task")), Bold(Cyan(taskName)))
+		executeTask(doFile, taskName)
 	}
+
+	fmt.Println(Bold(Green("Done executing all tasks for")), doFile.Description)
 }
